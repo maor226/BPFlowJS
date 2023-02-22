5ad7d482097e3dc1c218c70c7c47c2ea264016f4
maor226 <bitme@post.bgu.ac.il>
2023-02-22 17:33:57 +0200

    set rest mapper on helper class

--- src/main/java/com/example/bpflowjs/Helper.java
+++ src/main/java/com/example/bpflowjs/Helper.java
@@ -1,27 +1,34 @@
 package com.example.bpflowjs;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+@RestController
 public class Helper implements brige {
 
-    @Override
+    @GetMapping("/send_diagram")
     public boolean send_diagram(String diagram) {
         // TODO Auto-generated method stub
         return false;
     }
 
+    @GetMapping("/run_step")
     public DiagramRunStep run_step() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    @GetMapping("/run_diagram")
     public List<DiagramRunStep> run_diagram() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public  boolean update_diagram(String diagram) {
+    @GetMapping("/update_diagram")
+    public boolean update_diagram(String diagram) {
         // TODO Auto-generated method stub
         return false;
     }
