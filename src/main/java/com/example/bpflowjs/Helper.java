f1eca63f340f3f500af6bf557f96cd76a8e38cfc
ShalevHak <shalev.haklai@gmail.com>
2023-02-26 19:39:20 +0200

    Implements run_step()

--- src/main/java/com/example/bpflowjs/Helper.java
+++ src/main/java/com/example/bpflowjs/Helper.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Helper implements brige {
 
+    private static Integer stepIndex;
     @GetMapping("/send_diagram")
     public boolean send_diagram(String diagram) {
         // TODO Auto-generated method stub
@@ -17,14 +19,21 @@ public class Helper implements brige {
 
     @GetMapping("/run_step")
     public DiagramRunStep run_step() {
-        // TODO Auto-generated method stub
-        return null;
+        List<DiagramRunStep> runStepsList =run_diagram();
+        DiagramRunStep runStep=null;
+        if (stepIndex==null)
+            stepIndex=0;
+        if (stepIndex< runStepsList.size())
+            runStep= runStepsList.get(stepIndex);
+            stepIndex++;
+        return runStep;
     }
 
     @GetMapping("/run_diagram")
     public List<DiagramRunStep> run_diagram() {
         // TODO Auto-generated method stub
-        return null;
+        List<DiagramRunStep> runStepsList=new ArrayList<DiagramRunStep>();
+        return runStepsList;
     }
 
     @GetMapping("/update_diagram")
